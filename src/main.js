a9d99aa28199e4f34762c4d42aeb03faa6b2cfdd
Rapiz <rapiz@foxmail.com>
2019-08-25 10:29:53 +0800

    update

--- src/main.js
+++ src/main.js
@@ -7,11 +7,7 @@ function createWindow() {
         webPreferences: {
             nodeIntegration: true,
         },
-        frame: false,
-        transparent: true
     })
     win.loadFile('src/index.html');
-//    win.webContents.openDevTools();
 }
-const onAppReady = createWindow;
-app.on('ready', () => setTimeout(onAppReady, 500));
+app.on('ready', createWindow);
